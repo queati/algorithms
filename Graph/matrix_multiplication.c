812873a4d79f891bd2cfa3e650abbb7c12a288f0
queati <queati@sina.com>
2014-05-25 09:17:42 +0800

    add matrix multiplication solution for all pairs shortest path problem

--- Graph/matrix_multiplication.c
+++ Graph/matrix_multiplication.c
@@ -29,19 +29,20 @@ void matrix_mult(struct Matrix_Graph *mat_graph)
 void extend_edge(struct Matrix_Graph *mat_graph)
 {
 	int i, j, k;
+	double dis1, dis2;
 
 	for (i = 1; i <= mat_graph->node_num; i++)
 		for (j = 1; j <= mat_graph->node_num; j++) {
-			for (k = 1; k <= mat_graph->node_num; k++)
-				if (mat_graph->shortest_matrix[i][k] != INT_MAX && \
-					mat_graph->adjcent_matrix[k][j] != INT_MAX && \
+			for (k = 1; k <= mat_graph->node_num; k++) {
+				dis1 = mat_graph->shortest_matrix[i][k]; 
+				dis2 = mat_graph->adjcent_matrix[k][j];
+				if (dis1 != INT_MAX && dis2 != INT_MAX && \
 					mat_graph->shortest_matrix[i][j] > \
-					mat_graph->shortest_matrix[i][k] + \
-					mat_graph->adjcent_matrix[k][j]) {
+					dis1 + dis2) {
 					mat_graph->shortest_matrix[i][j] = \
-					mat_graph->shortest_matrix[i][k] + \
-					mat_graph->adjcent_matrix[k][j];
+					dis1 + dis2;
 					mat_graph->precusor[i][j] = k;
 				}
+			}
 		}
 }
